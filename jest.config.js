929cac03e1c1290439a719b77373f42b2dfe655b
jill bhatt <bhattjill@gmail.com>
2025-02-04 19:50:54 +0530

    Resolved jest related path alias errors

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,26 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+// const { pathsToModuleNameMapper } = require('ts-jest');
+// const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
     testEnvironment: 'node',
     transform: {
         '^.+.tsx?$': ['ts-jest', {}]
     },
     clearMocks: true,
-    moduleFileExtensions: ['ts', 'js', 'json']
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    moduleDirectories: ['node_modules', 'src'],
+    // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    //     prefix: 'src'
+    // })
+    moduleNameMapper: {
+        '^@app': '<rootDir>/src/app/$1',
+        '^@config': '<rootDir>/src/config/$1',
+        '^@db/(.*)': '<rootDir>/src/db/$1',
+        '^@interfaces': '<rootDir>/src/interfaces/$1',
+        '^@middleware/(.*)': '<rootDir>/src/middleware/$1',
+        '^@modules/(.*)': '<rootDir>/src/modules/$1',
+        '^@router': '<rootDir>/src/router/$1'
+    }
 };
--- src/modules/Todo/controller/__tests__/controllers.test.ts
+++ src/modules/Todo/controller/__tests__/controllers.test.ts
@@ -1,5 +1,21 @@
+import 'reflect-metadata';
+import todosController from '..';
+import { mockNext, mockRequest, mockResponse } from '../__mocks__';
+
 describe('Check setup', () => {
     it('Should check controllers tests', () => {
         expect(1).toBe(1);
     });
+});
+
+describe('Create controller tests', () => {
+    it('Should fetch all tasks', async () => {
+        await todosController.fetchTodosAll(
+            mockRequest,
+            mockResponse,
+            mockNext
+        );
+    });
+
+    expect(mockResponse.json).toHaveBeenCalled();
 });
