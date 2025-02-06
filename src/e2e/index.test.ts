7665af1e25b91ef2d25eb1a768f0613bb0733e1e
jill bhatt <bhattjill@gmail.com>
2025-02-07 00:56:07 +0530

    Added basic supertests

---
+++ src/e2e/index.test.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { Application } from 'express';
+import request from 'supertest';
+import initExpressApp from '@app';
+import connection from '@db/connection';
+
+jest.setTimeout(30000);
+
+describe('Check supertest e2e', () => {
+    let app: Application;
+
+    beforeAll(async () => {
+        app = initExpressApp();
+
+        await connection.connectMongoDB();
+    });
+
+    it('Should check e2e setup', () => {
+        const addition = (a: number, b: number) => a + b;
+
+        const sum = addition(1, 2);
+
+        expect(sum).not.toBe(1);
+        expect(sum).toBe(3);
+    });
+
+    it('Should test the info route', async () => {
+        const response = await request(app).get('/info');
+
+        expect(response.status).toEqual(200);
+        expect(response.body.success).toEqual(true);
+    });
+
+    afterAll(async () => {
+        await connection.disconnectMongoDB();
+    });
+});
